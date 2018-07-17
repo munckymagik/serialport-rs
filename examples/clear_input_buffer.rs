f3a480b7347a73bb63092989bff27fdd5cc332f9
Dan Munckton <dan@cultivatehq.com>
2018-07-17 12:27:28 +0100

    [SQUASH] reduce code in Arduino example in clear_input_buffer.rs

--- examples/clear_input_buffer.rs
+++ examples/clear_input_buffer.rs
@@ -19,9 +19,7 @@
 //
 // void setup() {
 //     Serial.begin(9600);
-//     while (!Serial) {
-//         ; // wait for serial port to connect. Needed for native USB
-//     }
+//     while (!Serial); // wait for serial port to connect. Needed for native USB
 // }
 //
 // int iter = 0;
